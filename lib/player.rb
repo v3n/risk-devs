312d8ae046775f0ebde973e3b476f6895687a1b6
Jonathan Howard <j@hovverd.com>
2015-09-18 03:56:07 -0400

    initial commit of player with unit and territory allocation

---
+++ lib/player.rb
@@ -0,0 +1,64 @@
+class Player
+    attr_reader :num
+    attr_reader :starting_territories
+
+    attr_accessor :territories
+
+    def initialize(num, world)
+        @world = world
+        @territories = { }
+        @num = num
+    end
+
+    def init
+        @starting_territories = @territories.dup
+    end
+
+    ## @armies => number of armies
+    def allocate_units(armies)
+        t = @territories.values.map do | tt | # map all territories
+            t_mult = 0
+            raw_t_vals = tt.links.map do | stt | # map all adjacent territories
+                # currently evaluated adjacent territory
+                eval_tt = @world.territories[stt]
+
+                # ignore owned territories
+                if eval_tt.owner == tt.owner
+                    next nil
+                end
+
+                t_mult += 1
+
+                # higher number = more danger 
+                eval_tt.armies.to_f / tt.armies.to_f 
+            end.compact
+
+            [tt, (raw_t_vals.inject(:+) / raw_t_vals.size) * t_mult ]
+        end.sort { |a,b| a[1] <=> b[1] }
+
+        armies.times do 
+            t[0][0].armies += 1
+
+            t.sort { |a,b| a[1] <=> b[1] }
+        end
+    end
+
+    ## @t => Territory
+    def acquire_territory(t)
+        t.owner.remove_territory t if not t.owner.nil?
+
+        t.owner = self
+        t.armies = 0
+
+        @territories[t.name.downcase.to_sym] = t
+    end
+
+    ## @t => Territory
+    def remove_territory(t)
+        if @territories.keys.include? t
+            @territories[:t].owner = nil
+            @territories[:t].armies = 0
+            @territories.delete t
+        end
+    end
+end
