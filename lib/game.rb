f80fff894742382b9aa86e18f34dca99f0b782cb
Jonathan Howard <j@hovverd.com>
2015-09-18 03:56:36 -0400

    initial commit of game with setup method

---
+++ lib/game.rb
@@ -0,0 +1,68 @@
+require_relative 'battle.rb'
+require_relative 'world.rb'
+require_relative 'player.rb'
+
+class Game 
+    attr_reader :players
+    attr_reader :world
+
+    def initialize(players)
+        @world = World.new
+        @players = [ ]
+
+        players.times do |i|
+            @players << Player.new(i, @world)
+        end
+    end
+
+    def set_up
+        p = 0
+        t = @world.territories.keys.shuffle
+        
+        # allocate territories
+        t.size.times do |i|
+            @players[p].acquire_territory @world.territories[t[i]]
+
+            p = p + 1
+            p = 0 if p >= @players.size
+        end
+
+        # initial unit number
+        units = 50 - (5 * @players.size)
+
+        # allocate to each player
+        player_units = { }
+        @players.each { |p| player_units[p] = units }
+
+        # one unit to each territory
+        @players.each do |p|
+            p.territories.values.each do |t|
+                t.armies += 1
+                player_units[p] -= 1
+            end
+        end
+
+        units.times do 
+            @players.each do |p|
+                p.allocate_units 1 if player_units[p] > 0
+                player_units[p] -= 1
+            end
+        end
+
+        @players.each do |p|
+            p.init
+        end
+    end
+
+    def rate_territory(t)
+
+    end
+
+    def process(p)
+
+    end
+
+    def loop
+
+    end
+end
